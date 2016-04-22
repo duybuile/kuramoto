c7344c245f2c6fbcae4fac4c0173044344bc38c0
Duy Le Bui <duybuile@gmail.com>
2016-04-22 17:30:50 +0100

    Commit new data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kuramoto
+
+## fastgreedy.R
+The clustering result for the whole network of 500,000 data rows: poor performance, taking 3 hours to finish, result in 1 community only.
--- kuramoto.R
+++ kuramoto.R
@@ -20,24 +20,47 @@ tasks <- read.csv(file = paste(WORKSPACE, "effort_on_tasks.csv", sep = "/"), hea
 repos <- read.csv(file = paste(WORKSPACE, "effort_on_repos.csv", sep = "/"), header = TRUE, 
                  sep = ",", stringsAsFactors = FALSE, strip.white = TRUE)
 
+#--------- PROCESSING
+
+#
+jira <- subset(jira, !is.na(jira$Worker.Id) & !is.na(jira$Task.Id))
+
 # For testing
 
 set.seed(3223)
-jira_sample <- jira[sample(nrow(jira), nrow(jira))*0.05, ]
+jira_sample <- jira[sample(nrow(jira), nrow(jira)*0.5), ]
 
 jira_sample <- subset(jira_sample, select = c("Worker.Id", "Task.Id"))
 
 t2 <- as.data.frame(table(jira_sample))
+t2 <- subset(t2, t2$Freq > 0)
+
+jira <- subset(jira, select = c("Worker.Id", "Task.Id"))
+
+t3 <- as.data.frame(table(jira))
+t3 <- subset(t3, t3$Freq > 0)
 
 network <- graph.data.frame(t2, directed = FALSE)
+network <- graph.data.frame(t3, directed = FALSE)
 #plot(network)
 
-V(network)
-E(network)
+#V(network)
+#E(network)
 
 fc <- fastgreedy.community(network)
+modularity(fc)
 membership(fc)
-sizes(fc)
+fc_size <- sizes(fc)
+length(size)
+
+wt <- walktrap.community(network)
+modularity(wt)
+wt_size <- sizes(wt)
+
+save(network, file = "matrix.Rdata")
+
+#This is for the whole database (takes about 3 hours)
+# Result is not great as there is only one community
 
 melt( t, id.vars = "Worker.Id")
 
